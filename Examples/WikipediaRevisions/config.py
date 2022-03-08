02defbbe5521d777a28656f029a22b317d60d181
Shay Lapid <lapidshay@gmail.com>
2022-03-08 14:58:18 +0200

    Cleaned Wikipedia Revisions Example

--- Examples/WikipediaRevisions/config.py
+++ Examples/WikipediaRevisions/config.py
@@ -2,18 +2,10 @@ import os
 from xgboost import XGBClassifier
 from os.path import join
 
-
 DATA_PATHS = {
-	'example_1': {
-		'TRAIN_SET_COMMS': join('Data', 'NoCategory__TrainSet__2019-01-01_2019-07-01.json'),
-		'TEST_SET_COMMS': join('Data', 'NoCategory__TestSet__2019-01-01_2019-07-01.json')
-	},
-	'example_2': {
 		'TRAIN_SET_COMMS': join('Data', 'NoCategory__TrainSet__2016-01-01_2020-07-15.json'),
 		'TEST_SET_COMMS': join('Data', 'NoCategory__TestSet__2016-01-01_2020-07-15.json')
 	}
-}
-
 
 CONFIG = {
 	'community_partite_label': 'Page',
